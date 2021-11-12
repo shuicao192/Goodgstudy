7e02d7614893bc1ac45e638beb0a7cd740d86b51
shuicao192 <kigoss@126.com>
2021-11-12 13:56:20 +0800

    UncaughtException

---
+++ src/base/uncaughtexception/UseOwnUncaughtExceptionHandler.java
@@ -0,0 +1,22 @@
+package base.uncaughtexception;
+
+public class UseOwnUncaughtExceptionHandler implements Runnable{
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler("捕获器"));
+
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-1").start();
+        Thread.sleep(300);
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-2").start();
+        Thread.sleep(300);
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-3").start();
+        Thread.sleep(300);
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-4").start();
+    }
+
+
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+}
